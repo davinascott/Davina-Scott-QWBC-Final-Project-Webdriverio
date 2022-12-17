051fc8d54a80a6288d10495cf4df840fd06bd4c2
davinascott <davinascott0401@gmail.com>
2022-12-16 20:15:53 -0500

    added cart test cases

--- test/pageobjects/cart.page.js
+++ test/pageobjects/cart.page.js
@@ -1,4 +1,5 @@
 
+const { $ } = require('webdriverio/build/commands/browser');
 const Page = require('./page');
 
 class CartPage extends Page {
@@ -17,6 +18,46 @@ class CartPage extends Page {
 
     get closeCartBtn() {
         return $('//*[@id="snipcart"]/div/div/div[2]/div/button');
+    }
+
+    get cartFirstRemoveButton() {
+        return $('(//*[@title="Remove item"])[1]')
+    }
+
+    get cartFirstIncrementBtn() {
+        return $('(//*[@title="Increment quantity"])[1]')
+    }
+
+    get cartFirstDecrementButton() {
+        return $('(//*[@title="Decrement quantity"])[1]')
+    }
+
+    get firstItemQuantity() {
+        return $('(//*[@class="snipcart-item-quantity__quantity snipcart__font--std"]/span)[1]')
+    }
+
+    get checkOutBtn() {
+        return $('//*[@class="snipcart-button-primary snipcart-base-button is-icon-right"]')
+    }
+
+    get checkOutBtnTextCont() {
+        return $('//*[@class="snipcart-base-button__label"]')
+    }
+
+    get emptyCartMessage() {
+        return $('//*[@class="snipcart-empty-cart__title snipcart__font--secondary snipcart__font--xlarge snipcart__font--bold"]');
+    }
+
+    async removeItem() {
+        this.cartFirstRemoveButton.click();
+    }
+
+    async inreaseItem() {
+        this.cartFirstIncrementBtn.click();
+    }
+
+    async decreaseItem() {
+        this.cartFirstDecrementButton.click();
     }
 }
 module.exports = new CartPage();
---
+++ test/specs/cart.e2e.js
@@ -0,0 +1,68 @@
+const authenticationPage = require("../pageobjects/authentication.page");
+const galleryPage = require("../pageobjects/product.page");
+const detailPage = require("../pageobjects/detail.page");
+const cartPage = require("../pageobjects/cart.page");
+
+
+describe('Cart: ', async () => {
+
+    //const initialQuantity = cartPage.firstItemQuantity.getText();
+
+    before('Navigate to landing page and press sign in button', async () => {
+        await authenticationPage.open();
+        await authenticationPage.btnSignInOrRegister.click();
+        await expect(browser).toHaveUrlContaining("https://dev-mlluudmotpwoldtv.us.auth0.com/login?");
+        await expect(authenticationPage.btnSubmit).toBeExisting();
+        await expect(authenticationPage.btnSubmitText).toHaveTextContaining('LOG IN');
+        await authenticationPage.authenticate('test1@tester.com','Password1');
+        await expect(browser).toHaveUrlContaining("https://ui-automation-camp.vercel.app/products");
+        await galleryPage.firstProductLink.scrollIntoView();
+    });
+
+    after('Sign out of application', async () => {
+        //await browser.url('https://ui-automation-camp.vercel.app/products');
+        await galleryPage.signOutBtn.waitForExist();
+        await galleryPage.signOutBtn.click();
+        await expect(browser).toHaveUrlContaining("https://ui-automation-camp.vercel.app");
+    });
+
+    it('Should be able to increase the quantity of a cart item', async () => {
+        await expect(galleryPage.firstProductAddToCartBtn).toBeExisting();
+        await galleryPage.firstProductAddToCartBtn.click();
+        await browser.url('https://ui-automation-camp.vercel.app/products#/cart');
+        await cartPage.cartFirstProductContainer.waitForExist();
+        await expect(cartPage.firstItemQuantity).toHaveTextContaining("1");
+        await cartPage.inreaseItem();
+        await expect(cartPage.firstItemQuantity).toHaveTextContaining("2");
+        //await browser.url('https://ui-automation-camp.vercel.app/products');
+    });
+
+    it('Should have the checkout button visible', async () => {
+        // await expect(galleryPage.firstProductAddToCartBtn).toBeExisting();
+        // await galleryPage.firstProductAddToCartBtn.click();
+        await browser.url('https://ui-automation-camp.vercel.app/products#/cart');
+        await cartPage.cartFirstProductContainer.waitForExist();
+        // await expect(cartPage.firstItemQuantity).toHaveTextContaining("1");
+        // await cartPage.inreaseItem();
+        await expect(cartPage.firstItemQuantity).toHaveTextContaining("2");
+        await cartPage.checkOutBtn.waitForExist();
+        await expect(cartPage.checkOutBtnTextCont).toHaveTextContaining("Checkout");
+        //await browser.url('https://ui-automation-camp.vercel.app/products');
+    });
+
+    it('Should be able to remove item from cart', async () => {
+        // await expect(galleryPage.firstProductAddToCartBtn).toBeExisting();
+        // await galleryPage.firstProductAddToCartBtn.click();
+        await browser.url('https://ui-automation-camp.vercel.app/products#/cart');
+        await cartPage.cartFirstProductContainer.waitForExist();
+        // await expect(cartPage.firstItemQuantity).toHaveTextContaining("1");
+        // await cartPage.inreaseItem();
+        await expect(cartPage.firstItemQuantity).toHaveTextContaining("2");
+        await cartPage.cartFirstRemoveButton.waitForExist();
+        await cartPage.cartFirstRemoveButton.click();
+        await cartPage.emptyCartMessage.waitForExist();
+        await expect(cartPage.emptyCartMessage).toHaveTextContaining("Your cart is empty.");
+        await browser.url('https://ui-automation-camp.vercel.app/products');
+    });
+
+});
