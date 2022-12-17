b8239d1984cbd1f22de1bb8e8b7cfff47858455b
davinascott <davinascott0401@gmail.com>
2022-12-17 08:20:17 -0500

    sort test cases added

---
+++ test/data/sort.data.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        scenario: "Sort A-Z",
+        selection: "A to Z",
+        results : ['Dell Laptop','Gray Couch','HP Laptop','Mackbook Pro','Quality Blue Shoes','Quality Cargo Pants','Quality Fitted Hat','Quality Heal Shoes','Quality Hooded Sweatshirt','Quality Jeans Pants','Quality Kids Tshirt','Quality Mousepad','Quality Mug','Quality Pillow','Quality Pink Pants','Quality Stylish Shoes','Quality Sweatshirt','Quality Trucker Hat','Quality Tshirt','Quality Vneck','Red Couch','White Couch']
+    },
+    {
+        scenario: "Sort by category:Pants",
+        selection: "Pants",
+        results : ['Quality Cargo Pants','Quality Jeans Pants','Quality Pink Pants']
+    },
+    {
+        scenario: "should not be able to search using category keywords",
+        searchterm: "Hats",
+        results : []
+    },
+
+]
